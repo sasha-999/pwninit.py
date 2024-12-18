466331dd17a4b0ba5db899d608d6220d41f0f12c
sasha-999 <66535877+sasha-999@users.noreply.github.com>
2024-12-18 23:11:43 +0000

    Added get_needed_from_dynamic() and get_runpath() to elfutils.py

--- elfutils.py
+++ elfutils.py
@@ -29,11 +29,7 @@ def vaddr_to_offset(elf, vaddr):
 
 
 def get_segments(elf, p_type):
-    segments = []
-    for segment in elf.iter_segments():
-        if segment.header["p_type"] == p_type:
-            segments.append(segment)
-    return segments
+    return list(filter(lambda seg: seg.header["p_type"] == p_type, elf.iter_segments()))
 
 
 def get_segment(elf, p_type):
@@ -48,6 +44,12 @@ def get_interp_patch(elf):
     if interp:
         return interp.get_interp_name(), interp.header["p_offset"]
     return None
+
+def get_interp(elf):
+    interp = get_interp_patch(elf)
+    if interp is None:
+        return None
+    return interp[0]
 
 
 def get_dynamic(elf):
@@ -71,16 +73,35 @@ def get_needed_patches(elf):
     return needed
 
 
-def get_interp(elf):
-    interp = get_interp_patch(elf)
-    if interp is None:
-        return None
-    return interp[0]
+def get_needed_from_dynamic(dynamic):
+    needed_tags = filter(lambda tag: tag.entry["d_tag"] == "DT_NEEDED", dynamic.iter_tags())
+    return list(map(lambda tag: tag.needed, needed_tags))
 
 
 def get_needed(elf):
-    return list(name for name, off in get_needed_patches(elf))
+    dynamic = get_dynamic(elf)
+    if dynamic is None:
+        return []
+    return get_needed_from_dynamic(dynamic)
 
+
+def get_runpath_from_dynamic(dynamic):
+    runpath = []
+    rpath = []
+    for tag in dynamic.iter_tags():
+        type = tag.entry["d_tag"]
+        if type == "DT_RUNPATH":
+            runpath = tag.runpath.split(":")
+        elif type == "DT_RPATH":
+            rpath = tag.rpath.split(":")
+    return runpath or rpath
+
+
+def get_runpath(elf):
+    dynamic = get_dynamic(elf)
+    if dynamic is None:
+        return []
+    return get_runpath_from_dynamic(dynamic)
 
 def get_arch(elf):
     return {
